9ad7b1d5672af12b25f802dc435330cb83c5d705
SDRAGON0802 <100835720+SDRAGON0802@users.noreply.github.com>
2022-05-22 18:22:14 +0900

    Add files via upload

---
+++ 네이버쇼핑.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome(executable_path= "./chromedriver.exe")
+
+url = "https://shopping.naver.com/home/p/index.naver"
+driver.get(url)
+
+
+time.sleep(1)
+
+serch=driver.find_element_by_xpath('//*[@id="_verticalGnbModule"]/div/div/div[2]/div/div[2]/form/fieldset/div/input')
+serch.click()
+driver.find_element_by_xpath('//*[@id="_verticalGnbModule"]/div/div/div[2]/div/div[2]/form/fieldset/div/input').send_keys('커피')
+serch.send_keys(Keys.ENTER)
+
+serch=driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/div[2]/div[3]/a')
+serch.click()
+
+serch=driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/div[2]/div[3]/ul/li[1]/a')
+serch.click()
+
+serch=driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/a[4]')
+serch.click()
+SO=[]
+driver.execute_script("window.scrollTo(0, 10000)")
+driver.execute_script("window.scrollTo(10000, 0)")
+
+for j in range(1, 21):
+  K=len(SO)
+  try:
+    sort = driver.find_element_by_xpath(f'//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[{j}]/li/div/div[2]/div[3]/span[4]').text
+    SO.append(sort)
+    if len(SO)==K:
+        sort=driver.find_element_by_xpath(f'//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[{j}]/li/div/div[2]/div[3]/span[4]').text
+  except:
+    sort="NAN"
+    pass
+  if len(SO)==20:
+      break
+print(SO)
